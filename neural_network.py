8fa1ad148dd78d3865160e83641b935684774dee
Alix C <53212285+Axil12@users.noreply.github.com>
2024-12-18 16:03:10 +0100

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -29,113 +29,63 @@ class NeuralNework:
 
     def __init__(
         self,
-        nb_inputs: int,
-        hidden_layers: tuple[int],
-        nb_outputs: int,
+        dimensions: tuple[int],
         activation: types.FunctionType | None = None,
     ):
-        self.nb_inputs = nb_inputs
-        self.hidden_layers = hidden_layers
-        self.nb_outputs = nb_outputs
-        self.activation = activation or NeuralNework.relu
-
         self.weight_min_val = -1
         self.weight_max_val = 1
 
-        # Weights of the input->hidden[0] layer
-        self.whi = np.random.uniform(
-            self.weight_min_val,
-            self.weight_max_val,
-            (self.hidden_layers[0], self.nb_inputs + 1),
-        )
-
-        # Weights of the input->hidden[0] layer
-        self.whh_list = [
+        self.activation = activation or NeuralNework.relu
+        self.dims = dimensions
+        self.weights = [
             np.random.uniform(
                 self.weight_min_val,
                 self.weight_max_val,
-                (self.hidden_layers[i + 1], self.hidden_layers[i] + 1),
+                (self.dims[i + 1], self.dims[i] + 1),
             )
-            for i in range(len(self.hidden_layers) - 1)
+            for i in range(len(self.dims) - 1)
         ]
-
-        # Weights of the hidden[-1]->output layer
-        self.woh = np.random.uniform(
-            self.weight_min_val,
-            self.weight_max_val,
-            (self.nb_outputs, self.hidden_layers[-1] + 1),
-        )
 
     def output(self, x: np.ndarray) -> np.ndarray:
         """
         Takes an input vector, feeds it to the neural network, and returns an output vector.
         """
-        x = np.concatenate([x, [[1]]], axis=0)  # Adding bias
-
-        for weights in [self.whi] + self.whh_list:
+        for weights in self.weights:
+            x = np.concatenate([x, [[1]]], axis=0)  # Adding bias
             x = np.dot(weights, x)
             x = self.activation(x)
-            x = np.concatenate([x, [[1]]], axis=0)  # Adding bias
-
-        x = np.dot(self.woh, x)
-        x = self.activation(x)
         return x
 
     def get_layers_outputs(self, x: np.ndarray) -> list[np.ndarray]:
         result = []
-
-        x = np.concatenate([x, [[1]]], axis=0)  # Adding bias
-        result.append(x)
-
-        for weights in [self.whi] + self.whh_list:
-            x = np.dot(weights, x)
-            x = self.activation(x)
+        for weights in self.weights:
             x = np.concatenate([x, [[1]]], axis=0)  # Adding bias
             result.append(x)
-
-        x = np.dot(self.woh, x)
-        x = self.activation(x)
+            x = np.dot(weights, x)
+            x = self.activation(x)
         result.append(x)
         return result
 
     @staticmethod
     def crossover(net_1, net_2):
-        child = NeuralNework(
-            net_1.nb_inputs, net_1.hidden_layers, net_1.nb_outputs, net_1.activation
-        )
+        child = NeuralNework(net_1.dims, net_1.activation)
 
-        for i in range(child.whi.shape[0]):
-            for j in range(child.whi.shape[1]):
-                if random.random() < 0.5:
-                    child.whi[i][j] = net_1.whi[i][j]
-                else:
-                    child.whi[i][j] = net_2.whi[i][j]
-
-        for k in range(len(child.whh_list)):
-            for i in range(child.whh_list[k].shape[0]):
-                for j in range(child.whh_list[k].shape[1]):
+        for k in range(len(child.weights)):
+            for i in range(child.weights[k].shape[0]):
+                for j in range(child.weights[k].shape[1]):
                     if random.random() < 0.5:
-                        child.whh_list[k][i][j] = net_1.whh_list[k][i][j]
+                        child.weights[k][i][j] = net_1.weights[k][i][j]
                     else:
-                        child.whh_list[k][i][j] = net_2.whh_list[k][i][j]
-
-        for i in range(child.woh.shape[0]):
-            for j in range(child.woh.shape[1]):
-                if random.random() < 0.5:
-                    child.woh[i][j] = net_1.woh[i][j]
-                else:
-                    child.woh[i][j] = net_2.woh[i][j]
+                        child.weights[k][i][j] = net_2.weights[k][i][j]
 
         return child
 
     def clone(self):
-        clone = NeuralNework(
-            self.nb_inputs, self.hidden_layers, self.nb_outputs, self.activation
-        )
-        clone.whi = np.copy(self.whi)
-        for i in range(len(self.whh_list)):
-            clone.whh_list[i] = np.copy(self.whh_list[i])
-        clone.woh = np.copy(self.woh)
+        clone = NeuralNework(self.dims, self.activation)
+
+        for i in range(len(self.weights)):
+            clone.weights[i] = np.copy(self.weights[i])
+
         return clone
 
     def mutate(self, mutation_rate: float) -> None:
@@ -143,7 +93,7 @@ class NeuralNework:
         Randomly mutates the weights neural network.
         The mutation_rate dictates the probability for each weight to be randomized.
         """
-        for weight_matrix in [self.whi] + self.whh_list + [self.woh]:
+        for weight_matrix in self.weights:
             for i in range(0, weight_matrix.shape[0], 1):
                 for j in range(0, weight_matrix.shape[1], 1):
                     if random.random() < mutation_rate:
@@ -153,8 +103,11 @@ class NeuralNework:
 
 
 def main():
-    net = NeuralNework(3, (5, 7, 6), 2, activation=NeuralNework.softplus)
+    net = NeuralNework((3, 2), activation=NeuralNework.softplus)
     x = np.random.rand(3, 1)
+    net.clone()
+    NeuralNework.crossover(net, net)
+    net.mutate(0.5)
     print(net.output(x))
     print(np.argmax(net.output(x)))
 
