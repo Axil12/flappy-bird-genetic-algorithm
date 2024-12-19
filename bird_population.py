15c6f455bd814db49bbc11cfe98c642ded0e239b
Alix C <53212285+Axil12@users.noreply.github.com>
2024-12-19 11:59:44 +0100

    Update bird_population.py

--- bird_population.py
+++ bird_population.py
@@ -20,6 +20,8 @@ class BirdPopulation:
 
         self.population_size = population_size
         self.bird_sprite = bird_sprite
+        self.bird_neural_network_dims = neural_network_dims
+        self.bird_neural_network_activation = neural_network_activation
 
         self.birds = []
         for i in range(population_size):
@@ -75,15 +77,17 @@ class BirdPopulation:
         child.net = NeuralNetwork.crossover(parent_1.net, parent_2.net)
         return child
 
-    def mutate(self, mutation_rate: float) -> None:
-        for bird in self.birds:
-            bird.net.mutate(mutation_rate)
-
     def natural_selection(
-        self, mutation_rate: float = 0.01, allow_clone: bool = False
+        self,
+        mutation_rate: float = 0.01,
+        allow_clone: bool = False,
+        redraw_rate: float = 0.05,
+        keep_champion: bool = True,
     ) -> None:
-        new_bird_list = []
         self.calculate_fitness_sum()
+        new_bird_list = []
+
+        best_bird = max(self.birds, key=lambda b: b.fitness)
 
         for i in tqdm(
             range(len(self.birds)),
@@ -92,8 +96,37 @@ class BirdPopulation:
         ):
             parents = self.select_parents(allow_clone=allow_clone)
             child = self.reproduce(parents[0], parents[1])
+            child.fitness = (parents[0].fitness + parents[1].fitness) / 2
             new_bird_list.append(child)
 
+        # We delete the n weakest children and replace them by a completely new bird
+        if redraw_rate > 0:
+            n_redraws = int(redraw_rate * len(new_bird_list))
+            new_bird_list = sorted(new_bird_list, key=lambda b: b.fitness)
+            min_fitness = new_bird_list[0].fitness
+            for i in range(n_redraws):
+                bird = Bird(self.bird_sprite)
+                bird.net = NeuralNetwork(
+                    self.bird_neural_network_dims,
+                    activation=self.bird_neural_network_activation,
+                )
+                new_bird_list.append(bird)
+            new_bird_list = new_bird_list[n_redraws:]
+
+        for bird in new_bird_list:
+            bird.net.mutate(mutation_rate=mutation_rate)
+
+        # To keep the best of the previous generation to see if he is still the best
+        if keep_champion:
+            champion_bird = Bird(self.bird_sprite)
+            champion_bird.sprite = best_bird.sprite_alt
+            champion_bird.net = best_bird.net
+
+            wost_bird_of_new_gen = min(new_bird_list, key=lambda b: b.fitness)
+            idx = new_bird_list.index(wost_bird_of_new_gen)
+            new_bird_list[idx] = champion_bird
+
+        assert len(new_bird_list) == len(self.birds)
+
         self.birds = new_bird_list
-        self.mutate(mutation_rate=0.01)
         self.gen += 1
